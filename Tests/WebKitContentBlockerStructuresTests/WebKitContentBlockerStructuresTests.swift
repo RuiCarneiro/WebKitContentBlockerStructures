3490e8e76f77bb8e72da0751b6d3f1ccdfdf4f5c
Rui Carneiro <rui.nelson@hotmail.com>
2020-07-12 20:58:55 +0100

    added tests

--- Tests/WebKitContentBlockerStructuresTests/WebKitContentBlockerStructuresTests.swift
+++ Tests/WebKitContentBlockerStructuresTests/WebKitContentBlockerStructuresTests.swift
@@ -2,14 +2,102 @@ import XCTest
 @testable import WebKitContentBlockerStructures
 
 final class WebKitContentBlockerStructuresTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(WebKitContentBlockerStructures().text, "Hello, World!")
+    
+    func testActions() {
+        XCTAssertNil(WKBLRule.Action(action: .cssDisplayNone), "css display none must have a selector")
+        
+        let cssRule = WKBLRule.Action(selectorToNotDisplay: "#a")
+        XCTAssert(cssRule.action == .cssDisplayNone)
+        
+        let cssAction = WKBLRule.Action(selectorsToNotDisplay: ["#a", ".hide"])
+        
+        XCTAssert(cssAction.action == .cssDisplayNone)
+        XCTAssert(cssAction.selector == "#a, .hide")
+        
+        XCTAssertNil(WKBLRule.Action(action: .cssDisplayNone, selector: nil))
+        
+        XCTAssertNil(WKBLRule.Action(action: .blockCookies, selector: "#a"))
+        
+        XCTAssertNotNil(WKBLRule.Action(action: .block, selector: nil))
+        
     }
-
+    
+    func testTriggers() {
+        XCTAssertNil(WKBLRule.Trigger(urlFilter: nil,
+                                      caseSensitiveUrlFilter: true,
+                                      resourceTypes: nil,
+                                      loadTypes: nil,
+                                      ifDomain: nil,
+                                      unlessDomain: nil),
+                     "Case Sensitive and no filter")
+        
+        XCTAssertNil(WKBLRule.Trigger(urlFilter: nil,
+                                      caseSensitiveUrlFilter: false,
+                                      resourceTypes: nil,
+                                      loadTypes: nil,
+                                      ifDomain: nil,
+                                      unlessDomain: nil),
+                     "Case Sensitive and no filter")
+        
+        XCTAssertNil(WKBLRule.Trigger(urlFilter: nil,
+                                      caseSensitiveUrlFilter: nil,
+                                      resourceTypes: nil,
+                                      loadTypes: nil,
+                                      ifDomain: ["a.com"],
+                                      unlessDomain: ["b.com"]),
+                     "If domain and unless domain are mutually exclusive")
+        
+        let ifDomainCase = WKBLRule.Trigger(ifDomain: ["a.com"],
+                                            unlessDomain: nil)
+        
+        let unlessDomainCase = WKBLRule.Trigger(ifDomain: nil,
+                                                unlessDomain: ["b.com"])
+        
+        XCTAssertNotNil(ifDomainCase)
+        XCTAssertNotNil(unlessDomainCase)
+        
+        XCTAssert(ifDomainCase?.ifDomain?.first == "a.com")
+        XCTAssert(unlessDomainCase?.unlessDomain?.first == "b.com")
+        
+        let t = WKBLRule.Trigger(urlFilter: "a",
+                                 caseSensitiveUrlFilter: true,
+                                 resourceTypes: [.font, .document, .document],
+                                 loadTypes: [.firstParty, .firstParty, .thirdParty])!
+        
+        XCTAssert(t.urlFilter == "a")
+        XCTAssert(t.urlFilterIsCaseSensitive == true)
+        XCTAssert(t.resourceType?.count == 2)
+        XCTAssert(t.loadType?.count == 2)
+        
+    }
+    
+    func testJsonMaker() {
+        let rule = WKBLRule(trigger: WKBLRule.Trigger(urlFilter: #"http://"#)!,
+                            action: WKBLRule.Action(action: .blockCookies)!)
+        
+        let jsonData = WKBLRule.generateBlocklistJson(rules: [rule])
+        let json = String(data: jsonData, encoding: .utf8)!
+        print(json)
+        let expectedJson = #"""
+[
+  {
+    "trigger" : {
+      "url-filter" : "http:\/\/"
+    },
+    "action" : {
+      "action" : "block-cookies"
+    }
+  }
+]
+"""#
+        
+        XCTAssert(json == expectedJson)
+    }
+    
     static var allTests = [
-        ("testExample", testExample),
+        ("testTriggers", testTriggers),
+        ("testActions", testActions),
+        ("testJsonMaker", testJsonMaker)
     ]
+    
 }
